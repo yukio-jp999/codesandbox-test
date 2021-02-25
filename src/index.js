8476725847c922a7940c1e395d9f5a7ae5f92708
yukio-jp999 <63632634+yukio-jp999@users.noreply.github.com>
2021-02-25 16:43:13 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -66,3 +66,32 @@
 // // テンプレート文字列を用いた方法
 // const message2 = `私の名前は${name}です。年齢は${age}です。`;
 // console.log(message2);
+
+/**
+ * アロー関数
+ */
+// 従来の関数
+// function func1(str){
+//   return str;
+// }
+
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// アロー関数
+// const func2 = (str) => {
+//   // const func2 = str => {
+//   return str;
+// };
+// const func2 = (str) => str;
+
+// console.log(func2("func2です"));
+
+// // const func3 = (num1, num2) => {
+// //   return num1 + num2;
+// // };
+// const func3 = (num1, num2) => num1 + num2;
+
+// console.log(func3(10, 20));
