2dc96c587988196fce5371dc7a856b7839f83a29
yukio-jp999 <63632634+yukio-jp999@users.noreply.github.com>
2021-02-25 16:32:13 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -23,8 +23,8 @@
 // // letは再宣言不可能
 // //let val2 = "let変数を再宣言";
 
-const val3 = "const変数";
-console.log(val3);
+// const val3 = "const変数";
+// console.log(val3);
 
 // constは上書きが不可能
 // val3 = "const変数を上書き";
@@ -43,10 +43,26 @@ console.log(val3);
 // console.log(val4);
 
 // constで定義した配列はプロパティの変更が可能
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
 
 // Reactは変数はほとんどconst使う
 // 処理の中で変数の更新が必要な場合はlet
+
+/**
+ * テンプレート文字列
+ */
+// const name = "じゃけぇ";
+// const age = 28;
+
+// // 「私の名前はじゃけぇです。年齢は28歳です。」
+
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
