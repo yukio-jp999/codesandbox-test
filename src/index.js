bccd71ae64e6e35262b75ba929c29b76e13dc2ff
yukio-jp999 <63632634+yukio-jp999@users.noreply.github.com>
2021-02-25 16:56:51 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -95,3 +95,27 @@
 // const func3 = (num1, num2) => num1 + num2;
 
 // console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "おじ",
+//   age: 37
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です.年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myProfile = ["じゃけぇ", 28];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です!。年齢は${age}です。`;
+// console.log(message4);
