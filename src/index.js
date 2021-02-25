831ca94fdb6dcde0ed5fa17a3de2a40229bd46c1
yukio-jp999 <63632634+yukio-jp999@users.noreply.github.com>
2021-02-25 17:02:37 +0900

    デフォルト値、引数など

--- src/index.js
+++ src/index.js
@@ -119,3 +119,11 @@
 // const [name, age] = myProfile;
 // const message4 = `名前は${name}です!。年齢は${age}です。`;
 // console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+// const sayHello = (name) => console.log(`こんにちは!${name}さん!`);
+// 初期値あり
+// const sayHello = (name="おじ") => console.log(`こんにちは!${name}さん!`);
+// sayHello("");
