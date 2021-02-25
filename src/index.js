3b01b7050a1c0111515f4c88c60784c7815cdc3b
yukio-jp999 <63632634+yukio-jp999@users.noreply.github.com>
2021-02-25 19:32:45 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -198,3 +198,23 @@
 // });
 
 // console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// // ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 < 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+// const num = 1300;
+// // toLocaleStringで3桁区切りにする
+// console.log(num.toLocaleString());
+
+// // numの方がnumberのとき
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています!!" : "許容範囲内です";
+// };
+// console.log(checkSum(50, 40));
