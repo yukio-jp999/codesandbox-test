113e238dd1f69102b2bb2049567d4fbb6662fec3
yukio-jp999 <63632634+yukio-jp999@users.noreply.github.com>
2021-02-25 17:16:12 +0900

    配列のコピー、結合

--- src/index.js
+++ src/index.js
@@ -127,3 +127,36 @@
 // 初期値あり
 // const sayHello = (name="おじ") => console.log(`こんにちは!${name}さん!`);
 // sayHello("");
+
+/**
+ * スプレッド構文 ...
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// // sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// イコールで配列コピーすると、同じところを参照してしまうため、
+// コピー先の配列を変更するとコピー元の配列も変更されてしまう
+// スプレット構文で書くと、コピー元は影響を受けない
